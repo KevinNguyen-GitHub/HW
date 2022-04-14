2c9df9fa6408f92e72472dc671d771b7661399e7
KevinNguyen-GitHub <80585016+KevinNguyen-GitHub@users.noreply.github.com>
2022-04-14 14:18:55 -0700

    changes

--- test.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.io.*;
-public class test{
-    public static void main(String[] args) {
-     double num = 11/2.0;
-     System.out.format("%.1f",num);
-    }
-  
-}
